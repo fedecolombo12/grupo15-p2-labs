8d72d71d8e92befe1e7c417ba21436a17d55553b
yabelneves <115899312+yabelneves@users.noreply.github.com>
2023-06-02 18:15:01 -0300

    BinarySearchTree

---
+++ src/uy/edu/um/prog2/adt/MyBinarySearchTree/src/MyBinarySearchTree/MyBinarySearchTree.java
@@ -0,0 +1,13 @@
+package MyBinarySearchTree;
+
+import java.util.List;
+
+public interface MyBinarySearchTree <K extends Comparable<K>, T> {
+
+    T find (K key);
+
+    void insert (K key, T data);
+
+    void delete (K key);
+
+}
---
+++ src/uy/edu/um/prog2/adt/MyBinarySearchTree/src/MyBinarySearchTree/MyBinarySearchTreeTest.java
@@ -0,0 +1,25 @@
+package MyBinarySearchTree;
+
+public class MyBinarySearchTreeTest {
+
+    public static void main(String[] args) {
+        MyBinarySearchTreeImpl<Integer,String> prueba = new MyBinarySearchTreeImpl<Integer, String>();
+
+        prueba.insert(6, "Perros");
+        prueba.insert(8,"Gatos");
+        prueba.insert(3,"Hamsters");
+        prueba.insert(1, "Pez");
+        prueba.insert(4, "Gallinas");
+
+        System.out.println(prueba.find(6)); //Devuelve perros, que es la información que tiene 6
+
+        System.out.println("PreOden: ");
+        prueba.preOrder();
+
+        System.out.println("InOrden: ");
+        prueba.inOrder();
+
+        System.out.println("PostOrden: ");
+        prueba.postOrden();
+    }
+}
